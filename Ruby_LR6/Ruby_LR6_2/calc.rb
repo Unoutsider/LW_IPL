4acd1649ba143c3f7bc19a33c22367e120f11af2
Unoutsider <sashabukhov2@yandex.ru>
2022-12-19 17:25:52 +0300

    Finish

--- Ruby_LR6/Ruby_LR6_2/calc.rb
+++ Ruby_LR6/Ruby_LR6_2/calc.rb
@@ -13,6 +13,33 @@ module Calc
   end
 
   # Метод средних прямоугольников
+  def generator(accur)
+    gen = []
+    n = 0
+    s = 0
+    Integer(1/accur).times do
+        n += 1
+        h = 1.fdiv n
+        cur_s = 0
+        n.times do |i|
+          x = i * h + (h / 2)
+          y = x / ((1 + (x**3))**0.5)
+          cur_s += y
+        end
+        cur_s *= h
+        s = cur_s
+        gen.append([s, n])
+    end
+    gen
+  end
+
+  def processor(accur)
+    exval = 0.429798384032304
+    generator(accur).find { |res| (res[0] - exval).abs <= accur}
+  end
+
+=begin
+  # Метод средних прямоугольников
   def processor(accur)
     exval = 0.429798384032304
     n = 0
@@ -32,4 +59,33 @@ module Calc
     end
     [s, n]
   end
+=end
+=begin
+  # Метод средних прямоугольников
+  def generator
+    gen = Enumerator.new do |y|
+      n = 0
+      s = 0
+      loop do
+        n += 1
+        h = 1.fdiv n
+        cur_s = 0
+        n.times do |i|
+          x = i * h + (h / 2)
+          y = x / ((1 + (x**3))**0.5)
+          cur_s += y
+        end
+        cur_s *= h
+        s = cur_s
+        y << [s, n]
+      end
+    end
+    gen.take(1000)
+  end
+
+  def processor(accur)
+    exval = 0.429798384032304
+    generator.find { |res| (res[0] - exval).abs <= accur}
+  end
+=end
 end
